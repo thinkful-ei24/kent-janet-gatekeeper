aea8d225534b9b1fa1bf3ffa2c05afced1db598b
Glitch (almond-textbook) <none>
2018-09-05 18:15:57 +0000

    🏋🚩 Checkpoint ./server.js:1725361/320

--- server.js
+++ server.js
@@ -69,12 +69,15 @@ function gateKeeper(req, res, next) {
   console.log(userPass);
   
   //req.user = USERS.find((user)=>{ user.userName === userPass.user});
-  for (let i = 0; i < USERS.length; i++){
-    if(userPass.user === USERS[i].userName && userPass.password === USERS.password){
-    req.user = USERS[i];
-    }
-  }
+  // for (let i = 0; i < USERS.length; i++){
+  //   if(userPass.user === USERS[i].userName && userPass.password === USERS.password){
+  //   req.user = USERS[i];
+  //   }
+  // }
   
+  req.user = USERS.find(function(user) {
+     return user.userName === userPass.user && user.password === userPass.pass;
+  });
 
   console.log(req.user);
   next();
